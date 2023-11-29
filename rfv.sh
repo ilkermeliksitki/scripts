0e8ae647ace74924a297e8d0c21a28393f135e1e
ilker m. sitki <ilkermeliksitki@proton.me>
2023-11-29 15:04:45 +0300

    add rfv (fuzzy finder version of rg)
    Changes to be committed:
    	new file:   rfv.sh

---
+++ rfv.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# solve correct history appending order problem
+# somehow we need to refresh the history of the current shell with `history -r'
+rg --color=always --line-number --no-heading --smart-case "${*:-}" |
+  fzf --ansi \
+      --color "hl:-1:underline,hl+:-1:underline:reverse" \
+      --delimiter : \
+      --preview 'batcat --color=always {1} --highlight-line {2}' \
+      --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
+      --bind 'ctrl-j:become(vim {1} +{2} && echo "vim {1} +{2}" | tee -a ~/.bash_history)'\
+
