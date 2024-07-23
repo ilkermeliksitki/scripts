120e1ef0d264cbf12be0fec5606254065a824a5f
ilker m. sitki <ilkermeliksitki@proton.me>
2024-07-23 15:37:21 +0200

    add approximate calculate finish time option
    Changes to be committed:
    	modified:   pomodoro.sh

--- pomodoro/pomodoro.sh
+++ pomodoro/pomodoro.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Define the durations (in minutes)
-FOCUS_TIME=1
-SHORT_BREAK=1
-LONG_BREAK=1
+FOCUS_TIME=25
+SHORT_BREAK=5
+LONG_BREAK=20
 
 # Define the number of Pomodoros before a long break
 POMODOROS_BEFORE_LONG_BREAK=4
@@ -62,12 +62,43 @@ while getopts ":f:" opt; do
     esac
 done
 
+# Calculate the finish time
+function calculate_finish_time {
+    local focus_time=$1
+    local short_break_time=$2
+    local long_break_time=$3
+    local total_focus_periods=$4
+    local pomodoros_before_long_break=$5
+
+    local total_seconds=0
+
+    # Calculate total duration in seconds
+    for ((i=1; i<=total_focus_periods; i++)); do
+        total_seconds=$(($total_seconds + $(minutes_to_seconds $focus_time)))
+        # Exclude the break after last focus time
+        if (( $i < $total_focus_periods )); then
+            # Choose long or short break
+            if (( $i % $pomodoros_before_long_break == 0 )); then
+                total_seconds=$(($total_seconds + $(minutes_to_seconds $long_break_time)))
+            else
+                total_seconds=$(($total_seconds + $(minutes_to_seconds $short_break_time)))
+            fi
+        fi
+    done
+
+    # Calculate finish time
+    local finish_time=$(date -d "+$total_seconds seconds" +%H:%M:%S)
+    echo "Expected finish time: $finish_time"
+}
+
 # Main Pomodoro loop
 function pomodoro {
     local pomodoros_completed=0
     local total_focus_periods=0
     local total_short_breaks=0
     local total_long_breaks=0
+
+    calculate_finish_time $FOCUS_TIME $SHORT_BREAK $LONG_BREAK $TOTAL_FOCUS_PERIODS $POMODOROS_BEFORE_LONG_BREAK
 
     while true; do
         # Focus period
