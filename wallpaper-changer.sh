da547aa81c0c00fae716a0a7f79215ba072300cb
ilker m. sitki <ilkermeliksitki@proton.me>
2024-10-03 18:13:47 +0200

    fixes #1
    instead of copying, rename the random image as main.jpg
    
     Changes to be committed:
    	modified:   wallpaper-changer.sh

--- wallpaper-changer.sh
+++ wallpaper-changer.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Directory with wallpapers
+# directory with wallpapers
 wallpaper_dir=~/Pictures/wallpapers
 
-# Choose a random image except main.jpg (the current wallpaper)
+# choose a random image except main.jpg (the current wallpaper)
 random_image=$(ls "$wallpaper_dir" | grep -v "main.jpg" | shuf -n 1)
-echo $random_image
 
-# Move the current wallpaper to a new random name
+# rename the current wallpaper as a new random name
 new_name=$(date +%s).jpg  # Use timestamp for a unique name
-mv "$wallpaper_dir/main.jpg" "$wallpaper_dir/$new_name"
+mv -v "$wallpaper_dir/main.jpg" "$wallpaper_dir/$new_name"
 
-# Copy the random image to main.jpg
-cp "$wallpaper_dir/$random_image" "$wallpaper_dir/main.jpg"
+# rename the random image as main.jpg
+mv -v "$wallpaper_dir/$random_image" "$wallpaper_dir/main.jpg"
 
