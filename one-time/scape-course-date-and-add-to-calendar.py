d64267763fdd4ab128adf468ae14e8d913fa7bb1
ilker m. sitki <ilkermeliksitki@proton.me>
2024-10-15 13:09:22 +0200

    add new files and modify script
    Changes to be committed:
    	new file:   bioengineering.html
    	new file:   biostatistics.html
    	modified:   scape-course-date-and-add-to-calendar.py

--- one-time/scape-course-date-and-add-to-calendar.py
+++ one-time/scape-course-date-and-add-to-calendar.py
@@ -1,15 +1,27 @@
-from bs4 import BeautifulSoup
 import re
+import requests
+from bs4 import BeautifulSoup
 
-with open('german-a1-1.html', 'r') as file:
+with open('biostatistics.html', 'r') as file:
     html = file.read()
 
 soup = BeautifulSoup(html, 'html.parser')
 appointments = soup.find_all('div', class_='appointment-compact slide-box')
 for appointment in appointments:
-    time = appointment.find('div', class_='apt-time').text # date  MON, 04.11.2024, 11:30 - 14:00  at  Room 002, Seminarraum (2903.EG.002)
-    date = re.search(r'\d{2}\.\d{2}\.\d{4}, \d{2}:\d{2}', time).group()
+    # date
+    apt_time = appointment.find('div', class_='apt-time').text # date  MON, 04.11.2024, 11:30 - 14:00  at  Room 002, Seminarraum (2903.EG.002)
+    date = re.search(r'\d{2}\.\d{2}\.\d{4}, \d{2}:\d{2}', apt_time).group()
+
+    # location
     loc = appointment.find('div', class_='apt-location')
-    href = "https://campus.tum.de/tumonline/ee/ui/ca2/" + loc.find('a')['href']
-    print(date)
-    print(href)
+    loc_href = None
+    if loc.find('a').get('href') is not None:
+        loc_href = "https://campus.tum.de/tumonline/ee/ui/ca2/" + loc.find('a').get('href')
+
+    # note
+    note = appointment.find('div', class_='appointment-comment ng-star-inserted')
+    if note:
+        pass
+    else:
+        note = None
+
