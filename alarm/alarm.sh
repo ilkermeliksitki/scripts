a18a1a7461f341152f4dd1b6500a5c6bf97b7489
ilker m. sitki <ilkermeliksitki@proton.me>
2025-04-22 14:57:16 +0200

    add alarm script
    Changes to be committed:
    	new file:   alarm.sh
    	new file:   voice.mp3

---
+++ alarm/alarm.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+# alarm.sh - Set an alarm with optional message
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <duration (e.g., 5*60)> [optional message]"
+    exit 1
+fi
+
+# evaluate time by using bc in seconds and catch error
+time_secs=$(echo "$1" | bc 2>/dev/null)
+if [ $time_secs -le 0 ]; then
+    echo "Error: Invalid duration, please use a number bigger than 0."
+    exit 1
+fi
+
+# message
+# remove the first argument with shift
+shift
+msg="${*:-⏰ Time\'s up!}"
+
+echo "alarm set for $time_secs seconds..."
+sleep "$time_secs"
+
+# notify
+notify-send "⏰ Alarm" "$msg" &
+paplay voice.mp3 2>/dev/null
+
