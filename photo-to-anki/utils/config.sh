f71112e22d89b4d591367735e92114808f50dfa2
ilker m. sitki <ilkermeliksitki@proton.me>
2025-08-15 16:24:53 +0200

    add some models to config.sh as commented out way
    On branch chat
    Changes to be committed:
    	modified:   utils/config.sh

--- photo-to-anki/utils/config.sh
+++ photo-to-anki/utils/config.sh
@@ -6,6 +6,9 @@ if [[ -z "$OPENAI_API_KEY" ]]; then
 fi
 export API_KEY=$OPENAI_API_KEY
 
-export MODEL="gpt-5-nano"            # gpt-5-nano input: $0.05 cached-inp: $0.005 output: $0.40
+#export MODEL="gpt-4o"                # gpt-4o     input: $2.50  cached-inp: $1.250 output: $10.00
+#export MODEL="gpt-5"                 # gpt-5      input: $1.25  cached-inp: $0.125 output: $10.00
+export MODEL="gpt-5-mini"             # gpt-5-mini input: $0.25  cached-inp: $0.025 output: $2.00
+#export MODEL="gpt-5-nano"            # gpt-5-nano input: $0.05  cached-inp: $0.005 output: $0.40
 export MAX_OUTPUT_TOKENS=5000
 
