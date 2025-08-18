118073abea32c2902a811a97ec69fff8633ee466
ilker m. sitki <ilkermeliksitki@proton.me>
2025-08-18 15:55:58 +0200

    increase the maximum output Changes to be committed: 	modified:   utils/config.sh

--- photo-to-anki/utils/config.sh
+++ photo-to-anki/utils/config.sh
@@ -10,5 +10,5 @@ export API_KEY=$OPENAI_API_KEY
 #export MODEL="gpt-5"                 # gpt-5      input: $1.25  cached-inp: $0.125 output: $10.00
 #export MODEL="gpt-5-mini"             # gpt-5-mini input: $0.25  cached-inp: $0.025 output: $2.00
 export MODEL="gpt-5-nano"            # gpt-5-nano input: $0.05  cached-inp: $0.005 output: $0.40
-export MAX_OUTPUT_TOKENS=5000
+export MAX_OUTPUT_TOKENS=10000
 
