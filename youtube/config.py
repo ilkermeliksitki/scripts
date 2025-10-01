a8f8a987cb6d92e370fb4886cc56304f53b3807a
ilker m. sitki <ilkermeliksitki@proton.me>
2025-10-01 23:21:26 +0300

    make some local imports and centralize paths
    Changes to be committed:
    	new file:   youtube/config.py
    	modified:   youtube/handlers.py

---
+++ youtube/config.py
@@ -0,0 +1,5 @@
+import os
+
+MUSIC_DIR = os.path.expanduser('~/Music/youtube')
+VIDEO_DIR = os.path.expanduser('~/Videos/youtube')
+
--- youtube/handlers.py
+++ youtube/handlers.py
@@ -2,6 +2,8 @@ import os
 
 from utils import parse_video_id, get_choice, print_video_info, sanitize_title, get_video_title
 from youtube_api import search_youtube
+from media_handler import download_audio, download_video, play_audio, play_video
+from config import MUSIC_DIR, VIDEO_DIR
 
 def handle_link(link, save, resolution):
     video_id = parse_video_id(link)
@@ -11,15 +13,13 @@ def handle_link(link, save, resolution):
 
     if choice == 'a':
         if save:
-            music_dir = os.path.expanduser('~/Music/youtube')
-            audio_path = download_audio(video_id, title, music_dir)
+            audio_path = download_audio(video_id, title, MUSIC_DIR)
             play_audio(audio_path, local=True)
         else:
             play_audio(link, local=False)
     elif choice == 'v':
         if save:
-            video_dir = os.path.expanduser('~/Videos/youtube')
-            video_path = download_video(video_id, title, resolution, video_dir)
+            video_path = download_video(video_id, title, resolution, VIDEO_DIR)
             play_video(video_path, local=True)
         else:
             play_video(link, resolution=resolution, local=False)
@@ -39,16 +39,14 @@ def handle_query(query, save, resolution):
         choice = get_choice('\na: audio only, v: video, enter: next (enter to continue): ', ['a', 'v'], default='')
 
         if choice == 'a':
-            music_dir = os.path.expanduser('~/Music/youtube')
             if save:
-                audio_path = download_audio(video['video_id'], title, music_dir)
+                audio_path = download_audio(video['video_id'], title, MUSIC_DIR)
                 play_audio(audio_path, local=True)
             else:
                 play_audio(url, local=False)
         elif choice == 'v':
-            video_dir = os.path.expanduser('~/Videos/youtube')
             if save:
-                video_path = download_video(video['video_id'], title, resolution, video_dir)
+                video_path = download_video(video['video_id'], title, resolution, VIDEO_DIR)
                 play_video(video_path, local=True)
             else:
                 play_video(url, resolution=resolution, local=False)
