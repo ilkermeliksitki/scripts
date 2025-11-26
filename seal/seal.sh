d488b84015d077034c05e8e3710fd0c9f3d2d03a
ilker m. sitki <ilkermeliksitki@proton.me>
2025-11-26 12:10:48 +0100

    seal: exclude the trailing slash for dir names
    Changes to be committed:
          modified:   seal.sh

--- seal/seal.sh
+++ seal/seal.sh
@@ -224,7 +224,7 @@ parse_lock_args() {
         fi
         ;;
       i )
-        excludes+=("$OPTARG")
+        excludes+=("$OPTARG%/") # strip trailing slash
         ;;
       \? )
         log_error "Invalid option: -$OPTARG"
